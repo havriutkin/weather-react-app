bbfd2d414ceffdda1fddbc67fe67137c4240893b
Vladyslav Havriutkin <vlad@Jays-iMac.local>
2023-10-15 00:04:34 -0400

    Add API KEY as env variable

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,16 +2,18 @@ import icon from './img/snow.png'
 import './App.css';
 
 import { useState, useEffect } from 'react';
+import { parseWeather } from './utils';
+
 
 function App() {
   const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const apiKey = ''; // Replace with your actual API key
+      const apiKey = process.env.REACT_APP_API_KEY;
       const params = new URLSearchParams({
         'key': apiKey,
-        'q': 'new york'
+        'q': 'Atlanta'
       });
 
       try {
@@ -21,8 +23,7 @@ function App() {
         }
 
         const weatherData = await response.json();
-        console.log(weatherData);
-        setWeather(weatherData);
+        setWeather(parseWeather(weatherData));
       } catch (error) {
         console.log(error);
       }
@@ -39,16 +40,16 @@ function App() {
   return (
     <div className="App h-screen flex items-center justify-center bg-white">
       <div className="flex flex-col h-1/2 w-1/2 border items-center justify-between pt-6 pb-1 pl-1 pr-1 bg-sky-600 rounded-md font-sans">
-        <img src={weather.current.condition.icon} className="h-1/3"></img>
+        <img src={weather.icon} className="h-1/3"></img>
         <div className="h-1/5 flex flex-col items-center justify-around">
-          <h2 className="w-max text-xl text-center">{weather.current.temp_f}&deg;</h2>
-          <h3 className="w-min text-xl text-center">{weather.current.condition.text}</h3>
+          <h2 className="w-max text-xl text-center">{weather.temp_f}&deg;</h2>
+          <h3 className="w-min text-xl text-center">{weather.text}</h3>
         </div>
         <div className="w-1/2 flex justify-evenly">
           <button>Toggle</button>
           <button>Toggle</button>
         </div>
-        <p className="self-start italic">Atlanta, Georgia, USA</p>
+        <p className="self-start italic">{weather.location}</p>
       </div>
     </div>
   );
