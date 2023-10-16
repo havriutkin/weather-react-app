f17c4a9026c6dca14191b0d96811fb29a7c28351
Vladyslav Havriutkin <vlad@Jays-iMac.local>
2023-10-16 01:28:47 -0400

    Implement Toggle buttons functionality. Implement dark theme

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -13,6 +13,8 @@ import Toggle from './Toggle';
 
 function App() {
   const [weather, setWeather] = useState(null);
+  const [isCelsius, setIsCelsius] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,26 +39,40 @@ function App() {
     fetchData();
   }, [])
 
+  const changeDegree = () => {
+    setIsCelsius(!isCelsius);
+  }
+
+  const changeTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  }
+
+
   if(!weather) return (
     <div className='App'>
       <h1>Loading...</h1>
     </div>
   )
 
+
   return (
-    <div className="App h-screen flex items-center justify-center bg-white">
-      <div className="h-2/3 w-3/4 md:h-1/2 md:w-1/2 lg:w-1/3 lg:h-2/3 flex flex-col items-center justify-around p-2 border border-slate-900 rounded-md shadow-lg shadow-cyan-500/80 bg-sky-600 font-sans animate-fade-in">
-        <img src={icon} className="h-1/2 w-2/3"></img>
-        <div className="w-3/4 md:w-2/3 h-1/3 flex items-center justify-between">
-          <div className="flex flex-col">
-            <p className="italic font-thin">{weather.location}</p>
-            <hr></hr>
-            <h3 className="w-max text-xl text-center font-light">{weather.text}</h3>
-            <h2 className="w-max text-3xl text-center font-bold">{weather.temp_f}&deg;</h2>
-          </div>
-          <div className="flex flex-col items-center justify-around">
-            <Toggle name={"themeToggle"} leftIcon={lightIcon} rightIcon={darkIcon}/>
-            <Toggle name={"degreeToggle"} leftIcon={fahrenheitIcon} rightIcon={celsiusIcon}/>
+    <div className={isDarkTheme ? "dark" : ""}>
+      <div className="h-screen flex items-center justify-center bg-white dark:bg-slate-900">
+        <div className="h-2/3 w-3/4 md:h-1/2 md:w-1/2 lg:w-1/3 lg:h-2/3 flex flex-col items-center justify-around p-2 border
+         border-slate-300 dark:border-slate-900 rounded-md shadow-lg shadow-cyan-500/80 bg-sky-600 font-sans animate-fade-in
+         dark:bg-sky-950 dark:text-white">
+          <img src={icon} className="h-1/2 w-2/3"></img>
+          <div className="w-3/4 md:w-2/3 h-1/3 flex items-center gap-5 md:justify-between">
+            <div className="flex flex-col">
+              <p className="italic font-thin">{weather.location}</p>
+              <hr className="dark:border-black"></hr>
+              <h3 className="w-max text-xl text-center font-light">{weather.text}</h3>
+              <h2 className="w-max text-3xl text-center font-bold">{isCelsius ? weather.temp_c : weather.temp_f}&deg;</h2>
+            </div>
+            <div className="flex flex-col items-center justify-around">
+              <Toggle name={"themeToggle"} leftIcon={lightIcon} rightIcon={darkIcon} onChange={changeTheme}/>
+              <Toggle name={"degreeToggle"} leftIcon={fahrenheitIcon} rightIcon={celsiusIcon} onChange={changeDegree}/>
+            </div>
           </div>
         </div>
       </div>
--- weather-app/src/Toggle.js
+++ weather-app/src/Toggle.js
@@ -10,7 +10,7 @@ const Toggle = ({name, leftIcon, rightIcon, iconScale, onChange}) => {
 
     return (
         <>
-            <label for={name} className="relative m-1 bg-gray-100 w-20 h-10 rounded-full cursor-pointer" >
+            <label htmlFor={name} className="relative m-1 bg-gray-100 dark:bg-gray-300 border border-black dark:border-white w-20 h-10 rounded-full cursor-pointer" >
                 <input type="checkbox" id={name} className="sr-only peer" onChange={handleChange}/>
                 <span className="w-2/5 h-4/5 absolute rounded-full left-1 top-1 peer-checked:left-11 transition-all duration-500 p-1">
                     {isCheked ? (
